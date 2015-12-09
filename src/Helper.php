adfc7b023fe8fefed0b17ed4aa0b39c46ffff98a
repat <repat@repat.de>
2015-12-09 16:40:26 +0100

    filled out functions

--- src/Helper.php
+++ src/Helper.php
@@ -6,7 +6,17 @@ use DB;
 class Helper {
 
   public static function str_wo_folders($str) {
-    return "";
+	$posOfLastSlash = strrpos ($str, "/");
+    return substr($str, $posOfLastSlash + 1, strlen($str));
+  }
+
+  public static function getMySQLHeaders($table) {
+     $tableContent = DB::table($table)->get();
+     $describeObjects = DB::select(DB::raw("DESCRIBE `" . $table . "`"));        
+     foreach ($describeObjects as $obj) {
+       $tableHeader[] = $obj->Field;
+     }
+  return $tableHeader;      
   }
 }
 
