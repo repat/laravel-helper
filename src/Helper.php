500b554555a82a2c1209ddb3a354f3a5832cfe24
repat <repat@repat.de>
2015-12-10 13:37:47 +0100

    assoc headers

--- src/Helper.php
+++ src/Helper.php
@@ -3,23 +3,35 @@ namespace repat\LaravelHelper;
 
 use DB;
 
-class Helper {
-
-  public static function str_wo_folders($str) {
-	$posOfLastSlash = strrpos ($str, "/");
-    return substr($str, $posOfLastSlash + 1, strlen($str));
-  }
-
-  public static function getMySQLHeaders($table) {
-     $describeObjects = DB::select(DB::raw("DESCRIBE `" . $table . "`"));
-     $tableHeader = array();
-     foreach ($describeObjects as $obj) {
-       $tableHeader[] = $obj->Field;
-     }
-     if (empty($tableHeader)){
-         return false;
-     }
-     return $tableHeader;
-  }
+class Helper
+{
+    
+    public static function str_wo_folders($str) {
+        $posOfLastSlash = strrpos($str, "/");
+        return substr($str, $posOfLastSlash + 1, strlen($str));
+    }
+    
+    public static function getMySQLHeaders($table, $assoc = false) {
+        $describeObjects = DB::select(DB::raw("DESCRIBE `" . $table . "`"));
+        $tableHeader = array();
+        if ($assoc) {
+            foreach ($describeObjects as $obj) {
+                $tableHeader[$obj->Field] = $obj->Field;
+            }
+        } 
+        else {
+            foreach ($describeObjects as $obj) {
+                $tableHeader[] = $obj->Field;
+            }
+        }
+        if (empty($tableHeader)) {
+            return false;
+        }
+        return $tableHeader;
+    }
+    
+    public static function getFilesFromFolder($folder) {
+        return array_diff(scandir($folder), array('..', '.'));
+    }
 }
 
