5a9d5224d1f82015438666a7fcb83b43a342b2a8
repat <repat@repat.de>
2015-12-13 17:31:24 +0100

    2 more methods

--- src/Helper.php
+++ src/Helper.php
@@ -6,6 +6,8 @@ use DB;
 class Helper
 {
     
+    const ASSOC_ARRAY = true;
+
     public static function str_wo_folders($str) {
         $posOfLastSlash = strrpos($str, "/");
         return substr($str, $posOfLastSlash + 1, strlen($str));
@@ -29,9 +31,21 @@ class Helper
         }
         return $tableHeader;
     }
-    
+
     public static function getFilesFromFolder($folder) {
         return array_diff(scandir($folder), array('..', '.'));
+    }
+
+    // the other method is more descriptive, but this resembles League\Flysystem
+    public static function files($folder) {
+       return self::getFilesFromFolder($folder);
+    }
+
+    // http://stackoverflow.com/a/20024776/2517690
+    public static function getNumberOfRows($file) {
+
+	$file->seek(PHP_INT_MAX);
+	return $file->key();
     }
 }
 
